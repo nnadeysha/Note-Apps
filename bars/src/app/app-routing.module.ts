874375957dbbefc89405414bf307ca424cd9b20b
novikova.n_qschool <nadeysha23@gmail.com>
2022-10-23 21:15:14 +0400

    feat: add main structure

--- bars/src/app/app-routing.module.ts
+++ bars/src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
+import { NotFoundComponent } from './components/not-found/not-found.component';
+import { LoginComponent } from './components/login/login.component';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', redirectTo: '/login', pathMatch: 'full'},
+  {path: 'login', component: LoginComponent},
+  {path: '**', component: NotFoundComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
---
+++ bars/src/app/services/auth.service.ts
@@ -0,0 +1,26 @@
+import { Injectable } from '@angular/core';
+import { Router } from '@angular/router';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthService {
+
+  constructor(private router: Router) { }
+
+  setToken(token: string) {
+    localStorage.setItem('token', token)
+  }
+
+  getToken() {
+    return localStorage.getItem('token')
+  }
+
+  isLoggedIn() {
+    return this.getToken() !== null;
+  }
+
+  login(userInfo) {
+    if (userInfo.email = 'admin@gmail.com' && )
+  }
+}
